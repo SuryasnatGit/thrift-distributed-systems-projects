4056d6aa7eb6439ff92479c8a21aa6e6ca09ce7f
Wen Chuan Lee <leex7095@umn.edu>
2016-02-29 02:19:21 -0600

    Fixed Ant file, this time it actually works... I hope.

--- assignment1/src/Node.thrift
+++ assignment1/src/Node.thrift
@@ -3,5 +3,5 @@ include "shared.thrift"
 service NodeService{
 	bool write(1: shared.Machine machine),
 	string read(1: string filename),
-	void updateDHT(1:list<node> nodeList)
+	void updateDHT(1:list<shared.Machine> nodeList)
 }
