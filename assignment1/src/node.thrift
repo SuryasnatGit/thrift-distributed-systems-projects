14231527e00d4a1714c6a8c56648311e788db2b0
Danh Nguyen <nguy1952@umn.edu>
2016-02-28 21:46:52 -0600

    yes

--- assignment1/src/node.thrift
+++ assignment1/src/node.thrift
@@ -1,13 +1,6 @@
-// I dunno whats in this
-struct node{
-	1: string ip,
-	2: i16 port,
-}
-
-
 
 service NodeService{
-	bool write(1: string ip,2: int port),
+	bool write(1: string ip,2: i16 port),
 	string read(1: string filename),
-	void updateDHT(1:list<node>)
+	void updateDHT(1:list<node> nodeList)
 }
