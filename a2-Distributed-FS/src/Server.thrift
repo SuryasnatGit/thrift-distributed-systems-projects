0bb519106de177c3a0119fd578075d458e5532d1
Danh Nguyen <nguy1952@umn.edu>
2016-03-24 14:59:13 -0500

    Prototype Server

--- a2-Distributed-FS/src/Server.thrift
+++ a2-Distributed-FS/src/Server.thrift
@@ -2,6 +2,7 @@ include "shared.thrift"
 
 service Server {
 	bool write(1: string filename 2: string contents),
-    	string read(1: string filename),
+    string read(1: string filename),
+    boolean enroll(1:Machine machine),
 	i32 ping(),
 }
