614305a2069fd459745229cdec40996b47745583
Danh Nguyen <nguy1952@umn.edu>
2016-04-13 17:10:17 -0500

    Thrift services

--- a3-map-reduce/ComputeNode.thrift
+++ a3-map-reduce/ComputeNode.thrift
@@ -1,5 +1,9 @@
 include "shared.thrift"
 
 service ComputeNode {
-    
+    string sort(1:string filename ,2: i32 start, 3: i32 end, 3: string ouput),
+    string merge(1:string f1, 2: string f2, 3: string output),
+    bool heartbeat(),
+    string getStats(),
+    bool cancel(1: string ouput),
 }
