f6c26dbb44c5b9c4d89f7ace381f9ccf76e8f629
Wen Chuan Lee <leex7095@umn.edu>
2016-04-15 01:12:32 -0500

    Calibration of file bytes implemented, added simple test file

--- a3-map-reduce/src/SortTask.java
+++ a3-map-reduce/src/SortTask.java
@@ -11,6 +11,14 @@ class SortTask extends Task {
 	this.output = output;
     }
 
+
+    public SortTask(long startOffset, long endOffset, String filename) {
+	this.startOffset = startOffset;
+	this.endOffset = endOffset;
+	this.filename = filename;
+	this.output = null;
+    }
+
     @Override
     public String toString() {
 	return "SORT: START " + startOffset + " END: " + endOffset + " FILE: " + filename + " OUT: " + output;
