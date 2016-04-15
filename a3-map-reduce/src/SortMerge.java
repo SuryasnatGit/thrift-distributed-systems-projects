cf4e5747bb27007960e247110e5bd3ba280c193a
Danh Nguyen <nguy1952@umn.edu>
2016-04-15 16:56:29 -0500

    That feel when u have to do Java IO

--- a3-map-reduce/src/SortMerge.java
+++ a3-map-reduce/src/SortMerge.java
@@ -3,6 +3,7 @@ import org.apache.thrift.TException;
 import org.apache.thrift.protocol.*;
 import org.apache.thrift.transport.*;
 import org.apache.thrift.server.*;
+import java.io.FilterOutputStream
  
 class SortMerge extends Thread { 
     Task task;
@@ -29,27 +30,54 @@ class SortMerge extends Thread {
         
         // Read uptil then endChunk
         
-        // Split by new line 
+        // Split by space
         
         // Convert to int arrayList
         
-        // collections sort
+        List<Integer> mockList = new ArrayList<>();
+        Collections.sort(mockList);
+        Writer wr = new FileWriter(new File(task.output));
         
-        // dump to output
-        
-        // return output
-        
+        for(Integer i: mockList){
+			wr.write(i + " ");
+		}
+		
+        wr.close();
         return false;
     }
    public boolean merge(MergeTask task) throws TException {
-        // Open both f1 and f2 using scanner
-        
-        // compare the next ints
-        
-        // write the next smallest
-        
-        // flush the file.
-        return false;
+	   Writer wr = new FileWriter(new File(task.output));
+	   Scanner sc1 = new Scanner(new File(task.f1));
+	   Scanner sc2 = new Scanner(new File(task.f2));
+	   
+		
+	   if(sc1.hasNextInt() && sc2.hasNextInt()){
+		   int a = sc1.nextInt();
+		   int b = sc2.nextInt();
+		   while (sc1.hasNextInt() && sc2.hasNextInt()) {
+			if(a < b){
+				wr.write(a + " ");
+				a.nextInt();
+			} else { 
+				wr.write(b + " ");
+				b.nextInt();
+			}
+		   }
+       }
+       
+       if(sc1.hasNextInt()){
+		   while(sc1.hasNextInt()){
+			   int c = sc1.nextInt();
+			   wr.write(c + " ");
+			}
+	   }else{
+			while(sc2.hasNextInt()){
+			   int c = sc2.nextInt();
+			   wr.write(c + " ");
+			}
+	   }
+       wr.close();
+       return false;
     }
 
 }
